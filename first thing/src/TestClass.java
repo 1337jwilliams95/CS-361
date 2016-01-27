1b5f91dc9ec17499f7007c82760357b2b25cb6ea
ceoii <ceoii@ceas-942-mac04.cae.uwm.edu>
2016-01-27 14:10:09 -0600

    rrr

--- first thing/src/TestClass.java
+++ first thing/src/TestClass.java
@@ -1,14 +1,13 @@
 
 public class TestClass {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 	System.out.print("we need to pick a team name");
 	System.out.println("you guys have any suggestions?");
-=======
+
 	System.out.print("test.");
 	System.out.print("test merge into main");
 	System.out.print("test update");
->>>>>>> branch 'master' of https://github.com/1337jwilliams95/CS-361.git
 	}
 	
 }
