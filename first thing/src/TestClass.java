b8afce4a71b2e39951bbd240246de242bf1b5f17
jbregall <jbregall@ceas-942-mac01.cae.uwm.edu>
2016-01-27 14:00:48 -0600

    commit temp

--- first thing/src/TestClass.java
+++ first thing/src/TestClass.java
@@ -2,6 +2,8 @@
 public class TestClass {
 	public static void main(String[] args) {
 	System.out.print("test.");
+	System.out.print("test merge into main");
+	System.out.print("test update");
 	}
 	
 }
