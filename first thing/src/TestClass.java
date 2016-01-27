bf960860c6f2fc01bb8f0479c1791acc20343b6e
ceoii <ceoii@ceas-942-mac04.cae.uwm.edu>
2016-01-27 14:07:05 -0600

    team stuff

--- first thing/src/TestClass.java
+++ first thing/src/TestClass.java
@@ -1,7 +1,8 @@
 
 public class TestClass {
 	public static void main(String[] args) {
-	System.out.print("test.");
+	System.out.print("we need to pick a team name");
+	System.out.println("you guys have any suggestions?");
 	}
 	
 }
